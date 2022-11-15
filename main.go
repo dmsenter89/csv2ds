8e214f2dbcc709cb6661e0a7f6c06a5b0f73ca5c
Michael Senter <dmsenter89@gmail.com>
2022-11-15 21:01:32 +0000

    First Working Template
    Squashed commit from devel.

--- main.go
+++ main.go
@@ -1,7 +1,190 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"encoding/csv"
+	"fmt"
+	"os"
+	"path"
+	"regexp"
+	"strings"
+)
+
+type CSVData struct {
+	dsName    string
+	header    []string
+	records   [][]string
+	isNumeric []bool
+}
 
 func main() {
-	fmt.Println("Initial commit line.")
+	var args int = len(os.Args)
+	var output string
+
+	if args > 1 { // process all files given in CLI
+		for i := range os.Args[1:] {
+			filename := os.Args[i+1]
+			ds := processFile(filename)
+			output += ds
+		}
+	} else {
+		usage()
+		os.Exit(1)
+	}
+
+	fmt.Println(output)
+
+}
+
+func usage() {
+	fmt.Printf("Usage: %s file1 [file2...]\n", path.Base(os.Args[0]))
+}
+
+func filenameWithoutExtension(filepath string) string {
+	var fileName string = path.Base(filepath)
+	var fileExtension string = path.Ext(filepath)
+	return strings.TrimSuffix(fileName, fileExtension)
+}
+
+// validateMemName returns a string that is
+// considered a SAS validmemname=compatible string.
+// This includes the following rules: 1) the length is
+// up to 32 characters, 2) the name may not
+// contain blanks or any special characters other than the
+// underscore, 3) names must begin with a Latin
+// alphabet character or an underscore.
+func validateMemName(sourceString string) string {
+	var compatibleName string = sourceString
+
+	// ensure that the membername is not more than 32 characters long
+	if len(sourceString) > 32 {
+		compatibleName = sourceString[:32]
+	}
+
+	var re = regexp.MustCompile(`(\W+)`)
+	compatibleName = re.ReplaceAllString(compatibleName, "_")
+
+	// must start with latin character or underscore
+	re = regexp.MustCompile(`(?m)^([^a-zA-Z_])`)
+	var substitution = "_$1"
+	compatibleName = re.ReplaceAllString(compatibleName, substitution)
+
+	return compatibleName
+}
+
+// collectColumnAsString Iterates over a particular column of the CSV
+// record and collects everything into a single long string. This is
+// a helper function to assist in determining if a particular column
+// is numeric or not using isStringOnlyNumeric.
+func collectColumnAsString(records [][]string, colNumber int) string {
+	var columnString string
+
+	// skip through the header row while accessing elements
+	for _, elem := range records[1:] {
+		columnString += elem[colNumber]
+	}
+
+	return columnString
+}
+
+// isStringOnlyNumeric parses the output of collectColumnAsString
+// and returns true if only valid numeric symbols are found,
+// false otherwise.
+func isStringOnlyNumeric(input string) bool {
+	var re = regexp.MustCompile(`[^\d\-\+\.]`)
+	// a match means that the string contains an unexpected symbol
+	// so we need to negate the bool value in the return.
+	return !re.MatchString(input)
+}
+
+// Given the name of the CSV file and the [][]string returned by the
+// CSV reader, initialize a CSVData element.
+func initializeCSVData(filename string, csvrecords [][]string) CSVData {
+	var data CSVData
+	data.dsName = validateMemName(filenameWithoutExtension(filename))
+	data.records = csvrecords[1:]
+
+	data.header = make([]string, len(csvrecords[0]))
+	data.isNumeric = make([]bool, len(data.header))
+
+	for i := range data.header {
+		data.header[i] = validateMemName(csvrecords[0][i])
+
+		columnValues := collectColumnAsString(data.records, i)
+		data.isNumeric[i] = isStringOnlyNumeric(columnValues)
+	}
+
+	return data
+}
+
+// writeDataStepFromCSVData uses the fields in CSVData to generate
+// a complete template data step that can be run in SAS.
+func writeDataStepFromCSVData(data CSVData) string {
+	var template string = fmt.Sprintf("data %s;\n", data.dsName)
+
+	template += fmt.Sprintln("\tinfile datalines DSD;")
+
+	template += fmt.Sprintf("\t%s\n", buildInputStatement(data.header, data.isNumeric))
+	template += fmt.Sprintln("\tdatalines;")
+
+	template += fmt.Sprint(buildDatalines(data.records))
+
+	template += ";\n"
+
+	return template
+}
+
+// buildInputStatement generates the input statement for the SAS data
+// step without preceeding tab or newline. Adds the '$' for after the
+// name of string variables.
+func buildInputStatement(header []string, isNumeric []bool) string {
+	var statement string = "input"
+
+	for i, name := range header {
+		statement += fmt.Sprintf(" %s", name)
+
+		if !isNumeric[i] {
+			statement += " $"
+		}
+	}
+
+	statement += ";"
+
+	return statement
+}
+
+// buildDatalines writes the CSV input for the actual records
+// back to a string for use in the datalines statement.
+func buildDatalines(records [][]string) string {
+	buf := new(bytes.Buffer)
+	w := csv.NewWriter(buf)
+	w.WriteAll(records)
+	return buf.String()
+}
+
+// processFile is the main driver of this program. It reads the relevant
+// file, initializes a CSVData object and generates the data step template.
+func processFile(filename string) string {
+	records := readCSV(filename)
+	data := initializeCSVData(filename, records)
+	ds := writeDataStepFromCSVData(data)
+
+	return ds
+}
+
+func readCSV(filepath string) [][]string {
+	f, err := os.Open(filepath)
+	if err != nil {
+		fmt.Printf("Error - cannot open file %s.\n", filepath)
+		os.Exit(2)
+	}
+	defer f.Close()
+	reader := csv.NewReader(f)
+	// r := csv.NewReader(strings.NewReader(os.Args[1]))
+	records, err := reader.ReadAll()
+	if err != nil {
+		fmt.Printf("Error - cannot read file %s.\n", filepath)
+		os.Exit(3)
+	}
+	return records
 }
---
+++ main_test.go
@@ -0,0 +1,231 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_filenameWithoutExtension(t *testing.T) {
+	type args struct {
+		filepath string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		// TODO: Add test cases.
+		{"No extension", args{"a"}, "a"},
+		{"Basic extension", args{"a.csv"}, "a"},
+		{"Path and extenson", args{"path/to/somewhere/a.csv"}, "a"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := filenameWithoutExtension(tt.args.filepath); got != tt.want {
+				t.Errorf("filenameWithoutExtension() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_validateMemName(t *testing.T) {
+	type args struct {
+		fileBase string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"Compatible name", args{"data1"}, "data1"},
+		{"Long compatible name", args{"Long_Name_That_Is_Compatible_Just_Too_Long"}, "Long_Name_That_Is_Compatible_Jus"},
+		{"Starts with a number", args{"45Name"}, "_45Name"},
+		{"Starts with a Special Char", args{"!name"}, "_name"},
+		{"Contains Spaces", args{"Name With Spaces"}, "Name_With_Spaces"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := validateMemName(tt.args.fileBase); got != tt.want {
+				t.Errorf("validateMemName() = '%v', want '%v'", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_collectColumnAsString(t *testing.T) {
+	sampleCSV := [][]string{{"Name", "Sex", "Age", "Height", "Weight"},
+		{"Alfred", "M", "14", "69", "112.5"},
+		{"Alice", "F", "13", "56.5", "84"},
+		{"Barbara", "F", "13", "65.3", "-98"}}
+
+	type args struct {
+		records   [][]string
+		colNumber int
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"Letters only", args{sampleCSV, 1}, "MFF"},
+		{"Ints Only", args{sampleCSV, 2}, "141313"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := collectColumnAsString(tt.args.records, tt.args.colNumber); got != tt.want {
+				t.Errorf("collectColumnAsString() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_isStringOnlyNumeric(t *testing.T) {
+
+	type args struct {
+		input string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{"Character string only", args{"AlfredAliceBarbara"}, false},
+		{"Ints only", args{"141313"}, true},
+		{"Ints and dollar sign", args{"1413$13"}, false},
+		{"Floats with signs", args{"69-56.5+65.3"}, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isStringOnlyNumeric(tt.args.input); got != tt.want {
+				t.Errorf("isStringOnlyNumeric() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_initializeCSVData(t *testing.T) {
+	simpleCSV := [][]string{{"Name", "Sex", "Age", "Height", "Weight"},
+		{"Alfred", "M", "14", "69", "112.5"},
+		{"Alice", "F", "13", "56.5", "84"},
+		{"Barbara", "F", "13", "65.3", "-98"}}
+	simpleRecords := [][]string{{"Alfred", "M", "14", "69", "112.5"},
+		{"Alice", "F", "13", "56.5", "84"},
+		{"Barbara", "F", "13", "65.3", "-98"}}
+	simpleHeader := []string{"Name", "Sex", "Age", "Height", "Weight"}
+	simpleNumeric := []bool{false, false, true, true, true}
+
+	harderCSV := [][]string{{"Name", "4Sex", "Age!", "Height", "$Weight"},
+		{"Alfred", "M", "14", "69", "112.5"},
+		{"Alice", "F", "13", "56.5", "84"},
+		{"Barbara", "F", "13", "65.3", "-98"}}
+	harderCSVheader := []string{"Name", "_4Sex", "Age_", "Height", "_Weight"}
+
+	type args struct {
+		filename   string
+		csvrecords [][]string
+	}
+	tests := []struct {
+		name string
+		args args
+		want CSVData
+	}{
+		{"Simple CSV", args{"sampleData", simpleCSV},
+			CSVData{"sampleData", simpleHeader, simpleRecords, simpleNumeric}},
+		{"Basic CSV, name to be fixed", args{"sample data", simpleCSV},
+			CSVData{"sample_data", simpleHeader, simpleRecords, simpleNumeric}},
+		{"Harder CSV", args{"!Bad$Name", harderCSV},
+			CSVData{"_Bad_Name", harderCSVheader, simpleRecords, simpleNumeric}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := initializeCSVData(tt.args.filename, tt.args.csvrecords); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("initializeCSVData() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_writeDataStepFromCSVData(t *testing.T) {
+
+	var solution string = `data sample;
+	infile datalines DSD;
+	input Name $ Sex $ Age Height Weight;
+	datalines;
+Alfred,M,14,69,112.5
+Alice,F,13,56.5,84
+Barbara,F,13,65.3,-98
+;
+`
+
+	type args struct {
+		data CSVData
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"Base Case", args{
+			CSVData{"sample",
+				[]string{"Name", "Sex", "Age", "Height", "Weight"},
+				[][]string{{"Alfred", "M", "14", "69", "112.5"},
+					{"Alice", "F", "13", "56.5", "84"},
+					{"Barbara", "F", "13", "65.3", "-98"}},
+				[]bool{false, false, true, true, true}},
+		}, solution},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := writeDataStepFromCSVData(tt.args.data); got != tt.want {
+				t.Errorf("writeDataStepFromCSVData() = \n`%v`,\n want \n`%v`", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_buildInputStatement(t *testing.T) {
+	type args struct {
+		header    []string
+		isNumeric []bool
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"Base case", args{[]string{"Name", "Sex", "Age", "Height", "Weight"}, []bool{false, false, true, true, true}},
+			"input Name $ Sex $ Age Height Weight;"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := buildInputStatement(tt.args.header, tt.args.isNumeric); got != tt.want {
+				t.Errorf("buildInputStatement() = `%v`, want `%v`", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_buildDatalines(t *testing.T) {
+	type args struct {
+		records [][]string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"Base case", args{[][]string{{"Alfred", "M", "14", "69", "112.5"},
+			{"Alice", "F", "13", "56.5", "84"},
+			{"Barbara", "F", "13", "65.3", "-98"}}},
+			`Alfred,M,14,69,112.5
+Alice,F,13,56.5,84
+Barbara,F,13,65.3,-98
+`}}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := buildDatalines(tt.args.records); got != tt.want {
+				t.Errorf("buildDatalines() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
